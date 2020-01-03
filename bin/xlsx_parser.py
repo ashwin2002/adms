26ec55c470adce78ee03fc2339afac865f870b96
ashwin <ashwintrojan+github@gmail.com>
2020-01-03 19:11:41 +0530

    Updating xlsx_parser to to b2b specific parsing

--- bin/xlsx_parser.py
+++ bin/xlsx_parser.py
@@ -38,21 +38,81 @@ class ExcelParser:
         worksheet_obj.append(data_row)
 
     def convert(self, input_file_name):
+        def get_month_index(month_str):
+            return ['', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul',
+                    'aug', 'sep', 'oct', 'nov', 'dec'].index(month_str)
+
+        def extract_non_b2b_data():
+            row_num = sheets_to_process[input_sheet_name]['start_row']
+            num_blank_rows = 0
+
+            self.appendRowToWorksheet(output_sheet, sheets_to_process[input_sheet_name]['header'])
+            col_range = [chr(i) for i in range(ord('A'), ord(sheets_to_process[input_sheet_name]['end_col'])+1)]
+            inv_num_index = sheets_to_process[input_sheet_name]['header'].index('INV No')
+            while True:
+                data_row = list()
+                for col in col_range:
+                    data_row.append(input_sheet[col + str(row_num)].value)
+
+                if ExcelParser.is_row_blank(data_row):
+                    num_blank_rows += 1
+                else:
+                    num_blank_rows = 0
+                    if len(data_row[0]) == 15 \
+                            and data_row[inv_num_index].find("-Total") == -1:
+                        self.appendRowToWorksheet(output_sheet, data_row)
+
+                if num_blank_rows == num_of_blank_rows_to_break:
+                    break
+                row_num += 1
+
+        def extract_b2b_data():
+            row_num = sheets_to_process[input_sheet_name]['start_row']
+            num_blank_rows = 0
+
+            self.appendRowToWorksheet(output_sheet, sheets_to_process[input_sheet_name]['header'])
+            col_range = ['A', 'C', 'E', 'F', 'G', 'H', 'D', 'I', 'J', 'K', 'L', 'M', 'N']
+
+            inv_num_index = sheets_to_process[input_sheet_name]['header'].index('INVNO')
+            while True:
+                data_row = list()
+                for index, col in enumerate(col_range):
+                    cell_val = input_sheet[col + str(row_num)].value
+                    if sheets_to_process[input_sheet_name]['header'][index] == "INVTYPE" \
+                            and cell_val == 'R':
+                        cell_val = "Regular"
+                    data_row.append(cell_val)
+
+                if ExcelParser.is_row_blank(data_row):
+                    num_blank_rows += 1
+                else:
+                    num_blank_rows = 0
+                    if len(data_row[0]) == 15 \
+                            and data_row[inv_num_index].find("-Total") == -1:
+                        supp_name = input_sheet['B%s' % row_num].value
+                        is_submitted = input_sheet['O%s' % row_num].value
+                        data_row.extend(['Inputs', 0.00, 0.00, 0.00, 0.00,
+                                         dwnload_val, gst_2yrm_val,
+                                         supp_name, is_submitted])
+                        self.appendRowToWorksheet(output_sheet, data_row)
+
+                if num_blank_rows == num_of_blank_rows_to_break:
+                    break
+                row_num += 1
+
         sheets_to_process = dict()
         sheets_to_process['B2B'] = dict()
         sheets_to_process['B2BA'] = dict()
         sheets_to_process['CDNR'] = dict()
         sheets_to_process['CDNRA'] = dict()
-        sheets_to_process['B2B']['check_dup_row'] = False
-        sheets_to_process['B2BA']['check_dup_row'] = False
-        sheets_to_process['CDNR']['check_dup_row'] = True
-        sheets_to_process['CDNRA']['check_dup_row'] = True
-        sheets_to_process['B2B']['header'] = ['GSTIN', 'INV No', 'INV Date', 'INV Value',
-                                              'Place', 'RCM', 'Rate', 'Taxable', 'IGST Paid',
-                                              'CGST Paid', 'SGST Paid', 'CESS Paid',
-                                              'Eligible', 'IGST availed', 'CGST availed',
-                                              'SGST availed', 'CESS availed', 'Download date',
-                                              'GST2YRM', 'Submited']
+
+        sheets_to_process['B2B']['header'] = ['GSTIN', 'INVNO', 'INVDATE', 'INVVALUE', 'PLACE',
+                                              'RCM', 'INVTYPE', 'RATE', 'TAXABLE', 'IGSTPAID',
+                                              'CGSTPAID', 'SGSTPAID', 'CESSPAID', 'ELIGIBLE',
+                                              'IGSTAVAILD', 'CGSTAVAILD', 'SIGSTAVAIL',
+                                              'CESSAVAILD', 'DWNLOAD', 'GST2YRM', 'SUPPNAME',
+                                              'SUBMITTED']
+
         sheets_to_process['B2BA']['header'] = ['GSTIN', 'SUPPNAME', 'INV No', 'INV Date',
                                                '', '', 'INV Value', 'Place', 'RCM',
                                                'INV Type', 'Rate', 'Taxable', 'IGST', 'CGST',
@@ -60,16 +120,16 @@ class ExcelParser:
                                                'SGST availed', 'CESS availed', 'Download date',
                                                'GST2YRM', 'Submitted']
         sheets_to_process['CDNR']['header'] = ['GSTIN', 'SUPP Name', 'Note Type', 'INV No',
-                                               'INV date', 'INV value', 'Reason', 'Rate',
+                                               'INV date', 'INV Value', 'Reason', 'Rate',
                                                'Taxable', 'IGST Paid', 'CGST Paid', 'SGST Paid',
                                                'CESS Paid', 'Submitted']
         sheets_to_process['CDNRA']['header'] = ['GSTIN', 'SUPP Name', 'Note Type', 'INV No',
-                                                'INV date', '', '', '', 'INV value', 'Place',
+                                                'INV date', '', '', '', 'INV Value', 'Place',
                                                 '', 'Rate',
                                                 'Taxable', 'IGST Paid', 'CGST Paid', 'SGST Paid',
                                                 'Submitted']
-        sheets_to_process['B2B']['start_row'] = 4
-        sheets_to_process['B2BA']['start_row'] = 5
+        sheets_to_process['B2B']['start_row'] = 7
+        sheets_to_process['B2BA']['start_row'] = 8
         sheets_to_process['CDNR']['start_row'] = 11
         sheets_to_process['CDNRA']['start_row'] = 10
         sheets_to_process['B2B']['end_col'] = 'V'
@@ -84,6 +144,12 @@ class ExcelParser:
         output_file_name = '/'.join(output_file_name)
         output_file_name += '/%s_parsed.xlsx' % file_name
         file_not_exists = self.checkFileExists(output_file_name, print_error=False)
+
+        file_name_data = input_file_name.split("/")[-1].split('.')[0].split('_')
+        gst_2yrm_val = file_name_data[1][2:] + file_name_data[1][0:2]
+        dwnload_val = str(file_name_data[3][0:2]) + '-' \
+                      + str(get_month_index(file_name_data[3][2:5].lower())) + '-' \
+                      + str(file_name_data[3][5:])
 
         try:
             # Load input / output workbooks
@@ -105,34 +171,10 @@ class ExcelParser:
                         output_workbook.remove(output_workbook.get_sheet_by_name(input_sheet_name))
                     input_sheet = input_workbook.get_sheet_by_name(input_sheet_name)
                     output_sheet = output_workbook.create_sheet(input_sheet_name)
-                    row_num = sheets_to_process[input_sheet_name]['start_row']
-                    num_blank_rows = 0
-                    prev_row = []
-
-                    self.appendRowToWorksheet(output_sheet, sheets_to_process[input_sheet_name]['header'])
-                    col_range = [chr(i) for i in range(ord('A'), ord(sheets_to_process[input_sheet_name]['end_col'])+1)]
-                    while True:
-                        data_row = list()
-                        for col in col_range:
-                            data_row.append(input_sheet[col + str(row_num)].value)
-
-                        if ExcelParser.is_row_blank(data_row):
-                            num_blank_rows += 1
-                        else:
-                            num_blank_rows = 0
-                            if len(data_row[0]) == 15:
-                                if sheets_to_process[input_sheet_name]['check_dup_row']:
-                                    if not prev_row \
-                                            or prev_row[0] != data_row[0]\
-                                            or prev_row[8] != data_row[8]:
-                                        self.appendRowToWorksheet(output_sheet, data_row)
-                                        prev_row = data_row
-                                else:
-                                    self.appendRowToWorksheet(output_sheet, data_row)
-
-                        if num_blank_rows == num_of_blank_rows_to_break:
-                            break
-                        row_num += 1
+                    if input_sheet_name == "B2B":
+                        extract_b2b_data()
+                    else:
+                        extract_non_b2b_data()
 
             output_workbook.save(output_file_name)
         except Exception as e:
